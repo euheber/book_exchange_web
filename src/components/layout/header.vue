157acb5105a944c75ceb9c404fc91aa89dedeb62
Heber Robert <euheber1@gmail.com>
2024-08-06 20:04:19 -0300

    create: logo and favicon

---
+++ src/components/layout/header.vue
@@ -0,0 +1,43 @@
+<template>
+   <header class=" w-full">
+      <nav class=" flex justify-between items-center p-2 max-w-6xl mx-auto">
+            <a href="#" class="text-xl cursor-pointer font-roboto text-emerald">Ecolib</a>
+
+            <ul class="flex gap-2 justify-center items-center">
+               <li>
+                  <a href="#" class="hover:text-emerald cursor-pointer transition-colors duration-200">Conheça o projeto</a>
+               </li>
+
+               <li>
+                  <a href="#" class="hover:text-emerald cursor-pointer transition-colors duration-200">Como funciona?</a>
+               </li>
+               <li class="cursor-pointer border bg-emerald text-white hover:text-black rounded-2xl py-1 px-2 duration-200">
+                  <a href="#" class="">Limpe sua estante</a>
+               </li>
+            </ul>
+      </nav>
+
+   </header>
+</template>
+
+
+<script setup>
+// import { onMounted, ref } from 'vue';
+
+// const formattedDate = ref('')
+// const dayNames = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+
+// onMounted(() => {
+//    const date = new Date();
+//    const formatted = date.toLocaleDateString('pt-BR');
+//    const dayName = dayNames[date.getDay()]
+
+//    formattedDate.value = `${dayName} | ${formatted}`
+   
+// });
+
+
+
+</script>
+
+
---
+++ src/views/home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import leaf from '@/components/icons/leaf.vue';
+</script>
+
+<template>
+  <main class="max-w-6xl p-5 mx-auto">
+      <div class="border max-w-80 bg-black p-5 flex justify-center">
+        <h1 class="text-emerald text-9xl font-bold font-roboto">ECO<br>LIB<leaf class="inline-block"/></h1> 
+      </div>
+  </main>
+</template>
+
+
