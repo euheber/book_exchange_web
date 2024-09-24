787167bb1295df91c2d467bf4bd45af0d3e1fcd6
Heber Robert <euheber1@gmail.com>
2024-09-24 18:25:52 -0300

    form transtion

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,36 +1,44 @@
 <script setup>
 import logo from '@/components/icons/logo.vue';
-import tree from '@/components/icons/tree.vue';
-import book from '@/components/icons/book.vue';
+
 import inputComponent from '@/components/layout/input.vue';
-import { ref, reactive } from 'vue';
+import { reactive, ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 // import { RouterLink } from 'vue-router';
-
+const router = useRouter()
 const userInfo = reactive({
   email: "",
   state: "",
   name: ""
 })
 
+let formOk = ref(true)
+
+const sendData = (e) => {  
+    e.preventDefault()
+
+
+    formOk.value = !formOk
+}
+
 </script>
 
 <template>
   
   <main class="bg-white overflow-auto">
-
-    
-
     <logo class="mt-6 mx-auto"/>
-    <form class="mt-10 mx-auto max-w-96 flex justify-center flex-col gap-2">
+    <form class="mt-10 mx-auto max-w-96 flex justify-center flex-col gap-2" v-if="formOk">
       <inputComponent v-model:value="userInfo.name" placeholder="Nome" class="border rounded-md p-2" /> 
 
       <inputComponent v-model:value="userInfo.email" placeholder="Email" class="border rounded-md p-2" /> 
 
       <inputComponent v-model:value="userInfo.state" placeholder="Estado" class="border rounded-md p-2" /> 
-      <button class="bg-emerald p-2 hover:bg-emerald-hover transition duration-250 rounded-md" @click="console.log('olá')">Enviar</button>
+      <button class="bg-emerald p-2 hover:bg-emerald-hover transition duration-250 rounded-md" @click="sendData">Enviar</button>
     </form>
-
+    <div v-if="!formOk">
+      <h1 class="font-exo">Check your email for more information!</h1>
+    </div>
   
   </main>
 </template>
