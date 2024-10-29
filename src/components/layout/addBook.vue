c5d471160af0c33ea8cea3b4c44705317506b36a
Heber Robert <euheber1@gmail.com>
2024-10-29 19:52:41 -0300

    fix: dinamic form field

--- src/components/layout/addBook.vue
+++ src/components/layout/addBook.vue
@@ -1,57 +1,65 @@
 <template>
    
-        <div :class="[isActive ? activeClass :  notActiveClass]">
+        <div :class="[isActive ? activeClass :  notActiveClass]" class="relative">
                <div v-if="isActive">
-                <label for="bookName">Nome do livro</label>
-                <inputComponent class="border w-full" id="bookName" v-model="bookInfo.name"/>
+                <label :for="'name-' + props.inputId">Nome do livro</label>
+                <inputComponent class="border w-full" :id="'name-' + props.inputId" v-model="bookInfo.name"/>
+                
+                <label :for="'isbn-' + props.inputId" class="block mt-5">ISBN</label>
+                <inputComponent class="border block w-full" :id="'isbn-' + props.inputId" v-model="bookInfo.isbn"/>
 
-                <label for="isbn" class="block mt-5">ISBN</label>
-                <inputComponent class="border block w-full" id="isbn" v-model="bookInfo.isbn"/>
-
-                <label for="publisher" class="block mt-5">Editora</label>
-                <inputComponent class="border block w-full " id="publisher" v-model="bookInfo.publisher"/>
-
+                <label :for="'publisher-' + props.inputId" class="block mt-5">Editora</label>
+                <inputComponent class="border block w-full " :id="'publisher-' + props.inputId" v-model="bookInfo.publisher"/>
+                
+                <p>{{ props.inputId }}</p>
                 <button class="mx-auto font-exo text-lg bg-emerald hover:bg-emerald-hover transition duration-250 p-2 w-48 rounded-md mt-10 block" @click="changeCardStyle">Adicionar</button>
                </div>
 
                <div v-if="!isActive" class="flex flex-col justify-evenly h-full p-2">
 
-                  <h1 class="font-exo text-4xl">{{ bookInfo.name }}</h1>
+                  <h1 class="font-exo text-2xl h-">{{ bookInfo.name }}</h1>
                   <p class="font-exo text-sm font-medium "> {{ bookInfo.publisher }}</p>
                   <div :class="barColorClass"></div>
-                  <p class="font-exo text-2xl text-end"> {{ bookInfo.isbn }}</p>
+                  <p class="font-exo text-2xl text-end mt-auto"> {{ bookInfo.isbn }}</p>
+            
                </div>
+
+               
+              <Transition name="fade">
+                <p class="p-4 bg-emerald absolute w-full bottom-0 right-0 rounded-b-md text-center" v-if="fieldWarn">Preencha todos os campos</p>
+              </Transition>
         </div>
         
 </template>
 
 <script setup>
 import inputComponent from '@/components/layout/input.vue';
+import { defineEmits, defineProps } from 'vue';
 import { reactive, ref } from 'vue';
 
-const bookInfo = reactive({
-    name: "",
-    isbn: "",
-    publisher: ""
-})
-
+const bookInfo = reactive({})
+const emit = defineEmits(['sendData'])
 let activeClass = ref('border rounded-md w-[500px] h-[400px]  p-2 place-content-center transition-all duration-250 mr-auto')
-let notActiveClass = ref('border rounded-md  w-[500px] max-h-44 mr-auto transition-all duration-250')
+let notActiveClass = ref('border rounded-md w-[500px] min-h-44 max-h-44 transition-all duration-250 overflow-y-auto')
 let barColorClass = ref("w-20 min-h-1  rounded-md bg-emerald mb-10")
-let booksList = reactive([])
 let isActive = ref(true)
+let fieldWarn = ref(false)
+const props = defineProps(['inputId'])
+
 
 
 const checkIfFieldIsEmpty = (obj) => { 
   const values = Object.values(obj)
-  return values.length < 3 
-
+  
+  return values.length < 3
 
 }
-const changeCardStyle = () => {
 
-  if(!checkIfFieldIsEmpty(bookInfo)){ 
-    console.log("preencha todos os campos")
+const changeCardStyle = () => {
+  
+  if(checkIfFieldIsEmpty(bookInfo)){ 
+    fieldWarn.value = true
+    setTimeout(() => { fieldWarn.value = false }, 3000)
     return
   }
 
@@ -59,8 +67,7 @@ const changeCardStyle = () => {
   const colors = [" orange", " purple", " green", " aqua-green", " yellow", " vibrant-purple", " sky-blue", " rosy-red", " mint-green", " magenta", " intense-blue", " gold-yellow", " neon-green", " burnt-orange"]
   barColorClass.value += colors[Math.floor(Math.random() * colors.length)]
 
-
-  booksList.push(bookInfo)
+  emit('sendData', bookInfo)
   isActive.value = !isActive.value
 }
 
@@ -68,7 +75,15 @@ const changeCardStyle = () => {
 
 
 <style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
 
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 
 </style>
 
--- src/views/registerBook.vue
+++ src/views/registerBook.vue
@@ -5,20 +5,18 @@ import addBook from '@/components/layout/addBook.vue';
 
 import { ref, reactive } from 'vue';
 
-const bookInfo = reactive({ 
-    bookName: "",
-    isbn: "",
-    publisher: ""
-})
 
-
-const bookList = reactive([])
+let bookList = ref([])
 
 const addComponent = (e) => { 
     e.preventDefault()
+    bookList.value.push(addBook)
+}
 
-    bookList.push(addBook)
-   
+
+const bookData = (book) => { 
+  
+    console.log(book)
 }
 </script>
 
@@ -29,15 +27,16 @@ const addComponent = (e) => {
 
             <h1 class="font-exo text-3xl mt-10"> ${userName}, preencha os campos com os dados dos livros que você está enviando</h1>
             <img src="/check_box.gif" alt="" class="mt-10">
-
+            
             <button class=" p-2 rounded-md bg-grey w-80 mx-auto h-[160px] block" @click="addComponent">Adicionar livro</button>
+            <button class=" p-2 rounded-md bg-emerald w-80 mx-auto h-[160px] block mt-10" >Enviar livros</button>
         </div>
 
  
 
-        <form  class="flex flex-wrap gap-2 h-screen align content-start overflow-y-auto max-w-[1100px] border p-4 justify-center" @submit.prevent>
+        <form  class="flex flex-wrap gap-2 h-screen content-start overflow-y-auto max-w-[1100px] border p-4" @submit.prevent>
        
-                <addBook v-for="books in bookList" :key="books.isbn"/>
+            <addBook v-for=" (books, index) in bookList" :key="index" :inputId="index" @sendData="bookData"/>
        
        
         </form>
